64a00d5b2c7f59cd587f59c57a0dd5a92a30cc79
Alexey Makeev <makeevhome@mail.ru>
2022-12-07 14:41:26 +0300

    feat: add new point render

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -4,6 +4,7 @@ import SortView from '../view/trip-sort.js';
 import EditPointView from '../view/edit-point.js';
 import PointView from '../view/trip-point.js';
 import FilterView from '../view/trip-filter.js';
+import NewPointView from '../view/new-point.js';
 
 export default class TripPresenter {
   MAX_POINT_COUNT = 3;
@@ -19,6 +20,7 @@ export default class TripPresenter {
     render(new SortView(), this.boardContainer);
     render(this.tripListComponent, this.boardContainer);
     render(new EditPointView(), this.tripListComponent.getElement());
+    render(new NewPointView(), this.tripListComponent.getElement());
 
     for (let i = 0; i < this.MAX_POINT_COUNT; i++) {
       render(new PointView(), this.tripListComponent.getElement());
