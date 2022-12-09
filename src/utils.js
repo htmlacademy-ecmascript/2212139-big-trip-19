b56d6379933c5b186b73346832b7095b9aa9f982
Alexey Makeev <makeevhome@mail.ru>
2022-12-09 13:21:05 +0300

    feat: update util.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 function getRandomArrayElement(items) {
   return items[Math.floor(Math.random() * items.length)];
 }
@@ -9,4 +11,30 @@ const getRandomInteger = (a, b) => {
   return Math.floor(min + Math.random() * (max - min + 1));
 };
 
-export { getRandomArrayElement, getRandomInteger };
+const humanizePointDueDate = (dueDate) => dayjs(dueDate).format('MMM D');
+
+const humanizePointDueTime = (dueDate) => dayjs(dueDate).format('HH:mm');
+
+const humanizePointDueDateAndTime = (dueDate) => dayjs(dueDate).format('DD/MM/YY HH:mm');
+
+const humanizePointDurationTime = (startDate, endDate) => {
+
+  const duration = dayjs.require('dayjs/plugin/duration');
+  dayjs.extend(duration);
+
+  const durationTime = dayjs(endDate).diff(startDate);
+  const durationTimeInHour = dayjs(endDate).diff(startDate, 'hour');
+
+  if (durationTimeInHour < 1) {
+    return dayjs.duration(durationTime).format('mm[M]');
+  }
+  if (durationTimeInHour >= 1 && durationTime < 24) {
+    return dayjs.duration(durationTime).format('HH[H] mm[M]');
+  }
+  if (durationTimeInHour >= 24) {
+    return dayjs.duration(durationTime).format('DD[D] HH[H] mm[M]');
+  }
+  return null;
+};
+
+export {getRandomArrayElement, getRandomInteger, humanizePointDueDate, humanizePointDueTime, humanizePointDurationTime, humanizePointDueDateAndTime};
