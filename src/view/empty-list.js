9e17272c25a4b6e9c52bbfd4c01a524e031bdbd4
Alexey Makeev <makeevhome@mail.ru>
2022-12-16 21:33:36 +0300

    init: start task 4.15

--- src/view/empty-list.js
+++ src/view/empty-list.js
@@ -1,10 +1,11 @@
 import { createElement } from '../render.js';
+import AbstractView from '../framework/view/abstract-view.js';
 
 function createEmptyListTemplate() {
   return '<p class="trip-events__msg">Click New Event to create your first point</p>';
 }
 
-export default class EmptyListView {
+export default class EmptyListView extends AbstractView {
 
   #element = null;
 
